da67e6823817f37217bdc9b19ac6d441a4a41dad
RoachOSL <os1996@o2.pl>
2025-03-29 12:21:35 +0100

    Initial project - first commit

---
+++ src/main/java/dev/soloprogramming/solocooking/SolocookingApplication.java
@@ -0,0 +1,13 @@
+package dev.soloprogramming.solocooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolocookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolocookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/soloprogramming/solocooking/SolocookingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.soloprogramming.solocooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolocookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
